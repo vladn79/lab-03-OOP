307f588f4a06d5a106294fa516b66b6a306d9d60
vladn79 <vladdd.nov@gmail.com>
2024-03-26 14:26:45 +0200

    finished 1 version of algorithm(need to delete dublicates)

--- recursive_alg/recursive_alg.cpp
+++ recursive_alg/recursive_alg.cpp
@@ -1,3 +1,12 @@
-//
-// Created by 38093 on 26.03.2024.
-//
+#include "ConvexHull.h"
+
+int main() {
+    Point a[] = {{2, 8}, {1, 1}, {9, 90}, {4, 4},
+                 {0, 0}, {1, 2}, {3, 1}, {3, 3}, {7, 12}, {5, 1}};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    ConvexHull convexHull;
+    convexHull.printHull(a, n);
+
+    return 0;
+}
