07aba9d986f32c0622e88419b15a037c60cd703e
vladn79 <vladdd.nov@gmail.com>
2024-03-27 23:29:07 +0200

    added 1 version of graham alg implementatione

--- graham_alg/convexhull_graham.cpp
+++ graham_alg/convexhull_graham.cpp
@@ -1,3 +1,77 @@
-//
-// Created by 38093 on 27.03.2024.
-//
+#include "convexhull_graham.h"
+
+Point_for_graham ConvexHullGraham::p0;
+
+Point_for_graham ConvexHullGraham::nextToTop(std::stack<Point_for_graham> &S) {
+    Point_for_graham p = S.top();
+    S.pop();
+    Point_for_graham res = S.top();
+    S.push(p);
+    return res;
+}
+
+int ConvexHullGraham::distSq( Point_for_graham p1, Point_for_graham  p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) +
+           (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+int ConvexHullGraham::orientation(Point_for_graham  p, Point_for_graham  q, Point_for_graham  r) {
+    int val = (q.y - p.y) * (r.x - q.x) -
+              (q.x - p.x) * (r.y - q.y);
+
+    if (val == 0)
+        return 0; // collinear
+    return (val > 0) ? 1 : 2; // clock or counterclockwise
+}
+
+int ConvexHullGraham::compare(const void *vp1, const void *vp2) {
+    Point_for_graham  *p1 = (Point_for_graham  *)vp1;
+    Point_for_graham  *p2 = (Point_for_graham  *)vp2;
+
+    int o = orientation(p0, *p1, *p2);
+    if (o == 0)
+        return (distSq(p0, *p2) >= distSq(p0, *p1)) ? -1 : 1;
+
+    return (o == 2) ? -1 : 1;
+}
+
+void ConvexHullGraham::convexHull(Point_for_graham  points[], int n) {
+    int ymin = points[0].y, min = 0;
+    for (int i = 1; i < n; i++) {
+        int y = points[i].y;
+        if ((y < ymin) || (ymin == y && points[i].x < points[min].x))
+            ymin = points[i].y, min = i;
+    }
+
+    std::swap(points[0], points[min]);
+    p0 = points[0];
+    qsort(&points[1], n - 1, sizeof(Point_for_graham ), compare);
+
+    int m = 1;
+    for (int i = 1; i < n; i++) {
+        while (i < n - 1 && orientation(p0, points[i], points[i + 1]) == 0)
+            i++;
+        points[m] = points[i];
+        m++;
+    }
+
+    if (m < 3)
+        return;
+
+    std::stack<Point_for_graham > S;
+    S.push(points[0]);
+    S.push(points[1]);
+    S.push(points[2]);
+
+    for (int i = 3; i < m; i++) {
+        while (S.size() > 1 && orientation(nextToTop(S), S.top(), points[i]) != 2)
+            S.pop();
+        S.push(points[i]);
+    }
+
+    while (!S.empty()) {
+        Point_for_graham  p = S.top();
+        std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
+        S.pop();
+    }
+}
--- graham_alg/convexhull_graham.h
+++ graham_alg/convexhull_graham.h
@@ -1,8 +1,21 @@
-//
-// Created by 38093 on 27.03.2024.
-//
-
 #ifndef LAB03_OOP_CONVEXHULL_GRAHAM_H
 #define LAB03_OOP_CONVEXHULL_GRAHAM_H
+
+#include <iostream>
+#include <stack>
+#include "point_for_graham.h"
+
+class ConvexHullGraham {
+private:
+    static Point_for_graham p0;
+
+    static Point_for_graham nextToTop(std::stack<Point_for_graham> &S);
+    static int distSq(Point_for_graham p1, Point_for_graham p2);
+    static int orientation(Point_for_graham p, Point_for_graham q, Point_for_graham r);
+    static int compare(const void *vp1, const void *vp2);
+
+public:
+    void convexHull(Point_for_graham points[], int n);
+};
 
 #endif //LAB03_OOP_CONVEXHULL_GRAHAM_H
