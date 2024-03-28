dc44efb3043d4cd049dad681547ea4cd3eeffbd9
vladn79 <vladdd.nov@gmail.com>
2024-03-28 21:25:44 +0200

    added point class file

---
+++ andrew_jarvis_alg/Point_for_Jarvis.h
@@ -0,0 +1,11 @@
+#ifndef LAB03_OOP_POINT_FOR_JARVIS_H
+#define LAB03_OOP_POINT_FOR_JARVIS_H
+
+class Point_for_Jarvis {
+public:
+    int x, y;
+    Point_for_Jarvis(int x = 0, int y = 0) : x(x), y(y) {}
+};
+
+
+#endif //LAB03_OOP_POINT_FOR_JARVIS_H
