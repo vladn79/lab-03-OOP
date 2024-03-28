0344ee9356f82f586fc38c6cf792678f5b6ba9a7
vladn79 <vladdd.nov@gmail.com>
2024-03-28 21:42:51 +0200

    added convex class file implementation/ orientation method

--- andrew_jarvis_alg/ConvexHullJarvis.cpp
+++ andrew_jarvis_alg/ConvexHullJarvis.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 
 
+
+
+
 int ConvexHullJarvis::orientation(Point_for_Jarvis p, Point_for_Jarvis q, Point_for_Jarvis r) {
     int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
     if (val == 0) return 0;
