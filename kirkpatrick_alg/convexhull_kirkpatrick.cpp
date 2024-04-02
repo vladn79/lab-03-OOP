d3fdfa2b0e9f3bef01abad4258c8d4864ec3c34c
vladn79 <vladdd.nov@gmail.com>
2024-04-02 23:22:57 +0300

    added convex hull class file implementation, orientation method

---
+++ kirkpatrick_alg/convexhull_kirkpatrick.cpp
@@ -0,0 +1,8 @@
+#include "convexhull_kirkpatrick.h"
+
+
+int ConvexHull::orientation(Point p, Point q, Point r) {
+    int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (val == 0) return 0;
+    return (val > 0) ? 1 : 2;
+}
