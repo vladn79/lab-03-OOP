c5bfb78518d1f4ab33d821c19231964580610e4e
vladn79 <vladdd.nov@gmail.com>
2024-04-03 20:34:00 +0300

    bags fixed

--- fortunes_alg/math.h
+++ fortunes_alg/math.h
@@ -1,69 +1,70 @@
-#ifndef LAB03_OOP_MATH_H
-#define LAB03_OOP_MATH_H
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <limits>
+#include <cmath>
 
-class Point_Fortunes{
-private:
-    double x_, y_;
+#define POINT_EPSILON 1.0e-6
 
-    struct CompareXY {
-        bool operator()(const Point_Fortunes &p1, const Point_Fortunes &p2) const;
+class Point_Fortunes {
+
+    struct Point_Fortunes_XY_Compare {
+        bool operator()(const Point_Fortunes &p1, const Point_Fortunes &p2) {
+            return (p1.x < p2.x || (p1.x == p2.x && p1.y < p2.y));
+        }
     };
 
 public:
-    static const double Inf;
-    static CompareXY xy_compare;
+    double x, y;
+
+    const static double Inf;
+    static Point_Fortunes_XY_Compare xy_compare;
 
     Point_Fortunes(double x = 0.0, double y = 0.0);
     Point_Fortunes(const Point_Fortunes &point);
 
-    Point_Fortunes& operator=(const Point_Fortunes& other);
-    bool operator==(const Point_Fortunes& other) const;
-    bool operator!=(const Point_Fortunes& other) const;
+    friend double dotProduct(const Point_Fortunes &p1, const Point_Fortunes &p2);
+    friend double crossProduct(const Point_Fortunes &p1, const Point_Fortunes &p2);
 
-    Point_Fortunes operator-() const;
-    Point_Fortunes operator+(const Point_Fortunes& other) const;
-    Point_Fortunes operator-(const Point_Fortunes& other) const;
-    Point_Fortunes operator*(double scalar) const;
-    Point_Fortunes operator/(double scalar) const;
+    friend Point_Fortunes operator+(const Point_Fortunes &p1, const Point_Fortunes &p2);
+    friend Point_Fortunes operator-(const Point_Fortunes &p1, const Point_Fortunes &p2);
+    friend Point_Fortunes operator/(const Point_Fortunes &p1, const Point_Fortunes &p2);
+    friend Point_Fortunes operator*(const Point_Fortunes &p, double value);
+    friend Point_Fortunes operator*(double value, const Point_Fortunes &p);
+    friend Point_Fortunes operator/(const Point_Fortunes &p, double value);
+    friend Point_Fortunes operator-(const Point_Fortunes &p);
 
-    Point_Fortunes& operator+=(const Point_Fortunes& other);
-    Point_Fortunes& operator-=(const Point_Fortunes& other);
-    Point_Fortunes& operator*=(double scalar);
-    Point_Fortunes& operator/=(double scalar);
+    friend std::ostream &operator<<(std::ostream &stream, const Point_Fortunes &p);
+    friend std::vector<Point_Fortunes> &operator<<(std::vector<Point_Fortunes> &v, const Point_Fortunes &p);
 
-    double operator[](int index) const;
+    Point_Fortunes &operator-=(const Point_Fortunes &p);
+    Point_Fortunes &operator+=(const Point_Fortunes &p);
+    Point_Fortunes &operator*=(double value);
+    Point_Fortunes &operator/=(double value);
 
-    friend ostream &operator<<(ostream &stream, const Point_Fortunes &p);
-    friend vector<Point_Fortunes> &operator<<(vector<Point_Fortunes> &v, const Point_Fortunes &p);
-
-    double getX() const;
-    double getY() const;
-    void setX(double x);
-    void setY(double y);
-
-    bool isVertical() const;
-    bool isHorizontal() const;
-    bool isValid() const;
-    double norm() const;
-    double norm2() const;
-
-    Point_Fortunes normalized() const;
+    Point_Fortunes normalized();
     void normalize();
+    double norm();
+    double norm2();
 
-    Point_Fortunes getRotated90CW() const;
-    Point_Fortunes getRotated90CCW() const;
+    Point_Fortunes getRotated90CW();
+    Point_Fortunes getRotated90CCW();
 
     static bool isLeftTurn(const Point_Fortunes &p1, const Point_Fortunes &p2, const Point_Fortunes &p3);
     static bool isRightTurn(const Point_Fortunes &p1, const Point_Fortunes &p2, const Point_Fortunes &p3);
+
+    double operator[](int i);
+
+    void setX(double x);
+    void setY(double y);
+
+    bool isVertical();
+    bool isHorizontal();
+    bool isValid();
+
 };
 
 double dotProduct(const Point_Fortunes &p1, const Point_Fortunes &p2);
 double crossProduct(const Point_Fortunes &p1, const Point_Fortunes &p2);
 
-bool equal(const Point_Fortunes &p1, const Point_Fortunes &p2, double EPSILON = 1.0e-6);
-bool equal(double v1, double v2, double EPSILON = 1.0e-6);
-
-#endif //LAB03_OOP_MATH_H
+bool equal(const Point_Fortunes &p1, const Point_Fortunes &p2, double EPSILON = POINT_EPSILON);
+bool equal(double v1, double v2, double EPSILON = POINT_EPSILON);
