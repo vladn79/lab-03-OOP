d07844d5beedceb6c27e31b4af840179171b703e
vladn79 <vladdd.nov@gmail.com>
2024-05-16 01:31:26 +0300

    added fortunes alg

--- fortunes_alg/realization.cpp
+++ fortunes_alg/realization.cpp
@@ -1,239 +1,305 @@
 #include "realization.h"
 #include "math.h"
 #include "data_structures.h"
+#include <list>
+#include <unordered_map>
 
-#include <queue>
+FortuneAlgorithm::FortuneAlgorithm(std::vector<Vector> points) : mDiagram(std::move(points))
+{
 
-#define BREAKPOINTS_EPSILON 1.0e-5
-#define _DEBUG_
+}
 
+FortuneAlgorithm::~FortuneAlgorithm() = default;
 
-namespace bl = beachline;
+void FortuneAlgorithm::construct()
+{
+    for (std::size_t i = 0; i < mDiagram.getNbSites(); ++i)
+        mEvents.push(std::make_unique<Event>(mDiagram.getSite(i)));
 
-
-struct Event {
-
-    enum { SITE = 0, CIRCLE = 1, SKIP = 2, };
-
-
-    int type;
-    Point_Fortunes point;
-
-
-    int index;
-
-
-    Point_Fortunes center;
-    bl::BLNodePtr arc;
-
-
-    Event(int _index = -1, int _type = Event::SKIP, const Point_Fortunes &_point = Point_Fortunes(0.0, 0.0)) :
-            index(_index), type(_type), point(_point), arc(nullptr) {}
-
-};
-
-
-typedef std::shared_ptr<Event> EventPtr;
-
-
-struct Point2DComparator {
-    bool operator()(const Point_Fortunes &p1, const Point_Fortunes &p2) {
-        return (p1.y == p2.y && p1.x > p2.x) || p1.y > p2.y;
+    while (!mEvents.isEmpty())
+    {
+        std::unique_ptr<Event> event = mEvents.pop();
+        mBeachlineY = event->y;
+        if(event->type == Event::Type::SITE)
+            handleSiteEvent(event.get());
+        else
+            handleCircleEvent(event.get());
     }
-};
+}
 
+VoronoiDiagram FortuneAlgorithm::getDiagram()
+{
+    return std::move(mDiagram);
+}
 
-struct Point2DComparator2 {
-    bool operator()(const Point_Fortunes &p1, const Point_Fortunes &p2) {
-        return (p1.y == p2.y && p1.x < p2.x) || p1.y < p2.y;
+void FortuneAlgorithm::handleSiteEvent(Event* event)
+{
+    VoronoiDiagram::Site* site = event->site;
+    // 1. Check if the bachline is empty
+    if (mBeachline.isEmpty())
+    {
+        mBeachline.setRoot(mBeachline.createArc(site));
+        return;
     }
-};
+    // 2. Look for the arc above the site
+    Arc* arcToBreak = mBeachline.locateArcAbove(site->point, mBeachlineY);
+    deleteEvent(arcToBreak);
+    // 3. Replace this arc by the new arcs
+    Arc* middleArc = breakArc(arcToBreak, site);
+    Arc* leftArc = middleArc->prev;
+    Arc* rightArc = middleArc->next;
+    // 4. Add an edge in the diagram
+    addEdge(leftArc, middleArc);
+    middleArc->rightHalfEdge = middleArc->leftHalfEdge;
+    rightArc->leftHalfEdge = leftArc->rightHalfEdge;
+    // 5. Check circle events
+    // Left triplet
+    if (!mBeachline.isNil(leftArc->prev))
+        addEvent(leftArc->prev, leftArc, middleArc);
+    // Right triplet
+    if (!mBeachline.isNil(rightArc->next))
+        addEvent(middleArc, rightArc, rightArc->next);
+}
 
+void FortuneAlgorithm::handleCircleEvent(Event* event)
+{
+    Vector point = event->point;
+    Arc* arc = event->arc;
+    // 1. Add vertex
+    VoronoiDiagram::Vertex* vertex = mDiagram.createVertex(point);
+    // 2. Delete all the events with this arc
+    Arc* leftArc = arc->prev;
+    Arc* rightArc = arc->next;
+    deleteEvent(leftArc);
+    deleteEvent(rightArc);
+    // 3. Update the beachline and the diagram
+    removeArc(arc, vertex);
+    // 4. Add new circle events
+    // Left triplet
+    if (!mBeachline.isNil(leftArc->prev))
+        addEvent(leftArc->prev, leftArc, rightArc);
+    // Right triplet
+    if (!mBeachline.isNil(rightArc->next))
+        addEvent(leftArc, rightArc, rightArc->next);
+}
 
-struct EventPtrComparator {
-    Point2DComparator point_cmp;
-    bool operator()(const EventPtr &e1, const EventPtr &e2) {
-        return point_cmp(e1->point, e2->point);
+Arc* FortuneAlgorithm::breakArc(Arc* arc, VoronoiDiagram::Site* site)
+{
+
+    Arc* middleArc = mBeachline.createArc(site);
+    Arc* leftArc = mBeachline.createArc(arc->site);
+    leftArc->leftHalfEdge = arc->leftHalfEdge;
+    Arc* rightArc = mBeachline.createArc(arc->site);
+    rightArc->rightHalfEdge = arc->rightHalfEdge;
+
+    mBeachline.replace(arc, middleArc);
+    mBeachline.insertBefore(middleArc, leftArc);
+    mBeachline.insertAfter(middleArc, rightArc);
+
+    delete arc;
+
+    return middleArc;
+}
+
+void FortuneAlgorithm::removeArc(Arc* arc, VoronoiDiagram::Vertex* vertex)
+{
+
+    setDestination(arc->prev, arc, vertex);
+    setDestination(arc, arc->next, vertex);
+
+    arc->leftHalfEdge->next = arc->rightHalfEdge;
+    arc->rightHalfEdge->prev = arc->leftHalfEdge;
+
+    mBeachline.remove(arc);
+
+    VoronoiDiagram::HalfEdge* prevHalfEdge = arc->prev->rightHalfEdge;
+    VoronoiDiagram::HalfEdge* nextHalfEdge = arc->next->leftHalfEdge;
+    addEdge(arc->prev, arc->next);
+    setOrigin(arc->prev, arc->next, vertex);
+    setPrevHalfEdge(arc->prev->rightHalfEdge, prevHalfEdge);
+    setPrevHalfEdge(nextHalfEdge, arc->next->leftHalfEdge);
+
+    delete arc;
+}
+
+bool FortuneAlgorithm::isMovingRight(const Arc* left, const Arc* right) const
+{
+    return left->site->point.y < right->site->point.y;
+}
+
+double FortuneAlgorithm::getInitialX(const Arc* left, const Arc* right, bool movingRight) const
+{
+    return movingRight ? left->site->point.x : right->site->point.x;
+}
+
+void FortuneAlgorithm::addEdge(Arc* left, Arc* right)
+{
+
+    left->rightHalfEdge = mDiagram.createHalfEdge(left->site->face);
+    right->leftHalfEdge = mDiagram.createHalfEdge(right->site->face);
+
+    left->rightHalfEdge->twin = right->leftHalfEdge;
+    right->leftHalfEdge->twin = left->rightHalfEdge;
+}
+
+void FortuneAlgorithm::setOrigin(Arc* left, Arc* right, VoronoiDiagram::Vertex* vertex)
+{
+    left->rightHalfEdge->destination = vertex;
+    right->leftHalfEdge->origin = vertex;
+}
+
+void FortuneAlgorithm::setDestination(Arc* left, Arc* right, VoronoiDiagram::Vertex* vertex)
+{
+    left->rightHalfEdge->origin = vertex;
+    right->leftHalfEdge->destination = vertex;
+}
+
+void FortuneAlgorithm::setPrevHalfEdge(VoronoiDiagram::HalfEdge* prev, VoronoiDiagram::HalfEdge* next)
+{
+    prev->next = next;
+    next->prev = prev;
+}
+
+void FortuneAlgorithm::addEvent(Arc* left, Arc* middle, Arc* right)
+{
+    double y;
+    Vector convergencePoint = computeConvergencePoint(left->site->point, middle->site->point, right->site->point, y);
+    bool isBelow = y <= mBeachlineY;
+    bool leftBreakpointMovingRight = isMovingRight(left, middle);
+    bool rightBreakpointMovingRight = isMovingRight(middle, right);
+    double leftInitialX = getInitialX(left, middle, leftBreakpointMovingRight);
+    double rightInitialX = getInitialX(middle, right, rightBreakpointMovingRight);
+    bool isValid =
+            ((leftBreakpointMovingRight && leftInitialX < convergencePoint.x) ||
+             (!leftBreakpointMovingRight && leftInitialX > convergencePoint.x)) &&
+            ((rightBreakpointMovingRight && rightInitialX < convergencePoint.x) ||
+             (!rightBreakpointMovingRight && rightInitialX > convergencePoint.x));
+    if (isValid && isBelow)
+    {
+        std::unique_ptr<Event> event = std::make_unique<Event>(y, convergencePoint, middle);
+        middle->event = event.get();
+        mEvents.push(std::move(event));
     }
-};
+}
 
-
-EventPtr checkCircleEvent(bl::BLNodePtr n1, bl::BLNodePtr n2, bl::BLNodePtr n3,
-                          const std::vector<Point_Fortunes> &points, double sweepline) {
-
-    if (n1 == nullptr || n2 == nullptr || n3 == nullptr)
-        return nullptr;
-
-    Point_Fortunes p1 = points[n1->get_id()];
-    Point_Fortunes p2 = points[n2->get_id()];
-    Point_Fortunes p3 = points[n3->get_id()];
-    Point_Fortunes center, bottom;
-
-    if (p2.y > p1.y && p2.y > p3.y)
-        return nullptr;
-
-    if (!findCircleCenter(p1, p2, p3, center))
-        return nullptr;
-
-    bottom = center;
-    bottom.y += (center - p2).norm();
-
-
-    if (fabs(bottom.y - sweepline) < POINT_EPSILON || sweepline < bottom.y) {
-
-        EventPtr e = std::make_shared<Event>(-1, Event::CIRCLE, bottom);
-
-        e->center = center;
-        e->arc = n2;
-
-        n2->circle_event = e;
-        return e;
+void FortuneAlgorithm::deleteEvent(Arc* arc)
+{
+    if (arc->event != nullptr)
+    {
+        mEvents.remove(arc->event->index);
+        arc->event = nullptr;
     }
+}
 
-    return nullptr;
+Vector FortuneAlgorithm::computeConvergencePoint(const Vector& point1, const Vector& point2, const Vector& point3, double& y) const
+{
+    Vector v1 = (point1 - point2).GetOrthogonal();
+    Vector v2 = (point2 - point3).GetOrthogonal();
+    Vector delta = 0.5 * (point3 - point1);
+    double t = delta.GetDet(v2) / v1.GetDet(v2);
+    Vector center = 0.5 * (point1 + point2) + t * v1;
+    double r = center.GetDistance(point1);
+    y = center.y - r;
+    return center;
 }
 
 
-void build_voronoi(const std::vector<Point_Fortunes> &points,
-                   std::vector<bl::HalfEdgePtr> &halfedges,
-                   std::vector<bl::VertexPtr> &vertices,
-                   std::vector<bl::HalfEdgePtr> &faces) {
 
-    std::priority_queue<EventPtr, std::vector<EventPtr>, EventPtrComparator> pq;
+bool FortuneAlgorithm::bound(Field box)
+{
 
-
-    for (size_t i = 0; i < points.size(); ++i) {
-        pq.push(std::make_shared<Event>(static_cast<int>(i), Event::SITE, points[i]));
+    for (const auto& vertex : mDiagram.getVertices())
+    {
+        box.left = std::min(vertex.point.x, box.left);
+        box.bottom = std::min(vertex.point.y, box.bottom);
+        box.right = std::max(vertex.point.x, box.right);
+        box.top = std::max(vertex.point.y, box.top);
     }
-    faces.resize(points.size(), nullptr);
 
-    bl::BLNodePtr root;
-    double sweepline = 0L;
+    std::list<LinkedVertex> linkedVertices;
+    std::unordered_map<std::size_t, std::array<LinkedVertex*, 8>> vertices(mDiagram.getNbSites());
+    if (!mBeachline.isEmpty())
+    {
+        Arc* leftArc = mBeachline.getLeftmostArc();
+        Arc* rightArc = leftArc->next;
+        while (!mBeachline.isNil(rightArc))
+        {
 
-    while (!pq.empty()) {
+            Vector direction = (leftArc->site->point - rightArc->site->point).GetOrthogonal();
+            Vector origin = (leftArc->site->point + rightArc->site->point) * 0.5f;
+            Field::Intersection intersection = box.getFirstIntersection(origin, direction);
 
-        EventPtr e = pq.top(); pq.pop();
+            VoronoiDiagram::Vertex* vertex = mDiagram.createVertex(intersection.point);
+            setDestination(leftArc, rightArc, vertex);
 
-        sweepline = e->point.y;
+            if (vertices.find(leftArc->site->index) == vertices.end())
+                vertices[leftArc->site->index].fill(nullptr);
+            if (vertices.find(rightArc->site->index) == vertices.end())
+                vertices[rightArc->site->index].fill(nullptr);
 
-        if (e->type == Event::SITE) {
+            linkedVertices.emplace_back(LinkedVertex{nullptr, vertex, leftArc->rightHalfEdge});
+            vertices[leftArc->site->index][2 * static_cast<int>(intersection.side) + 1] = &linkedVertices.back();
+            linkedVertices.emplace_back(LinkedVertex{rightArc->leftHalfEdge, vertex, nullptr});
+            vertices[rightArc->site->index][2 * static_cast<int>(intersection.side)] = &linkedVertices.back();
 
-            int point_i = e->index;
-            if (root == nullptr) {
-                root = std::make_shared<bl::BLNode>(std::make_pair(point_i, point_i), &sweepline, &points);
-            } else {
+            leftArc = rightArc;
+            rightArc = rightArc->next;
+        }
+    }
 
-                bl::BLNodePtr arc = bl::find(root, e->point.x);
-                bl::BLNodePtr subtree, left_leaf, right_leaf;
+    for (auto& kv : vertices)
+    {
+        auto& cellVertices = kv.second;
 
-                if (arc->circle_event != nullptr) {
-                    EventPtr circle_e = arc->circle_event;
-                    circle_e->type = Event::SKIP;
-                }
+        for (std::size_t i = 0; i < 5; ++i)
+        {
+            std::size_t side = i % 4;
+            std::size_t nextSide = (side + 1) % 4;
 
-                int isp_num = intersectionPointsNum(points[arc->get_id()], e->point, sweepline);
-
-                if (isp_num == 1) {
-                    subtree = bl::make_simple_subtree(point_i, arc->get_id(), &sweepline, &points, halfedges);
-                    left_leaf = subtree->left;
-                    right_leaf = subtree->right;
-                } else if (isp_num == 2) {
-                    subtree = bl::make_subtree(point_i, arc->get_id(), &sweepline, &points, halfedges);
-                    left_leaf = subtree->left;
-                    right_leaf = subtree->right->right;
-                } else {
-                    continue;
-                }
-
-                if (arc->prev != nullptr)
-                    bl::connect(arc->prev, left_leaf);
-
-                if (arc->next != nullptr)
-                    bl::connect(right_leaf, arc->next);
-
-                root = bl::replace(arc, subtree);
-
-                EventPtr circle_event = checkCircleEvent(left_leaf->prev, left_leaf, left_leaf->next, points, sweepline);
-                if (circle_event != nullptr) {
-                    pq.push(circle_event);
-                }
-                circle_event = checkCircleEvent(right_leaf->prev, right_leaf, right_leaf->next, points, sweepline);
-                if (circle_event != nullptr) {
-                    pq.push(circle_event);
-                }
+            if (cellVertices[2 * side] == nullptr && cellVertices[2 * side + 1] != nullptr)
+            {
+                std::size_t prevSide = (side + 3) % 4;
+                VoronoiDiagram::Vertex* corner = mDiagram.createCorner(box, static_cast<Field::Side>(side));
+                linkedVertices.emplace_back(LinkedVertex{nullptr, corner, nullptr});
+                cellVertices[2 * prevSide + 1] = &linkedVertices.back();
+                cellVertices[2 * side] = &linkedVertices.back();
             }
 
-        } else if (e->type == Event::CIRCLE) {
-
-            bl::BLNodePtr arc = e->arc, prev_leaf, next_leaf;
-            std::pair<bl::BLNodePtr, bl::BLNodePtr> breakpoints = bl::breakpoints(arc);
-            if (breakpoints.first == nullptr || breakpoints.second == nullptr) {
-                continue;
-            }
-
-            double v1 = breakpoints.first->value(), v2 = breakpoints.second->value();
-
-            if (fabs(v1 - v2) > BREAKPOINTS_EPSILON) {
-                continue;
-            }
-
-            bl::VertexPtr vertex = std::make_shared<bl::Vertex>(e->center);
-            bl::HalfEdgePtr h_first = breakpoints.first->edge;
-            bl::HalfEdgePtr h_second = breakpoints.second->edge;
-
-            vertices.push_back(vertex);
-            if (arc->prev != nullptr && arc->prev->circle_event != nullptr) {
-                EventPtr circle_e = arc->prev->circle_event;
-                circle_e->type = Event::SKIP; // ignore corresponding event
-            }
-            if (arc->next != nullptr && arc->next->circle_event != nullptr) {
-                EventPtr circle_e = arc->next->circle_event;
-                circle_e->type = Event::SKIP;
-            }
-            prev_leaf = arc->prev;
-            next_leaf = arc->next;
-
-            assert(prev_leaf != nullptr);
-            assert(next_leaf != nullptr);
-            bl::BLNodePtr new_edge_node;
-            if (arc->parent == breakpoints.first)
-                new_edge_node = breakpoints.second;
-            else
-                new_edge_node = breakpoints.first;
-            root = bl::remove(arc);
-            std::pair<bl::HalfEdgePtr, bl::HalfEdgePtr> twin_nodes = bl::make_twins(prev_leaf->get_id(), next_leaf->get_id());
-            new_edge_node->edge = twin_nodes.first;
-
-            bl::connect_halfedges(h_second, h_first->twin);
-            bl::connect_halfedges(h_first, twin_nodes.first);
-            bl::connect_halfedges(twin_nodes.second, h_second->twin);
-
-            h_first->vertex = vertex;
-            h_second->vertex = vertex;
-            twin_nodes.second->vertex = vertex;
-            vertex->edge = h_second;
-
-            halfedges.push_back(twin_nodes.first);
-            halfedges.push_back(twin_nodes.second);
-
-            if (prev_leaf != nullptr && next_leaf != nullptr) {
-                EventPtr circle_event = checkCircleEvent(prev_leaf->prev, prev_leaf, next_leaf, points, sweepline);
-                if (circle_event != nullptr) {
-                    pq.push(circle_event);
-                }
-                circle_event = checkCircleEvent(prev_leaf, next_leaf, next_leaf->next, points, sweepline);
-                if (circle_event != nullptr) {
-                    pq.push(circle_event);
-                }
+            else if (cellVertices[2 * side] != nullptr && cellVertices[2 * side + 1] == nullptr)
+            {
+                VoronoiDiagram::Vertex* corner = mDiagram.createCorner(box, static_cast<Field::Side>(nextSide));
+                linkedVertices.emplace_back(LinkedVertex{nullptr, corner, nullptr});
+                cellVertices[2 * side + 1] = &linkedVertices.back();
+                cellVertices[2 * nextSide] = &linkedVertices.back();
             }
         }
     }
 
-    for (size_t i = 0; i < halfedges.size(); ++i) {
-        bl::HalfEdgePtr he = halfedges[i];
-        if (he->prev == nullptr || faces[he->l_index] == nullptr) {
-            faces[he->l_index] = he;
+    for (auto& kv : vertices)
+    {
+        std::size_t i = kv.first;
+        auto& cellVertices = kv.second;
+        for (std::size_t side = 0; side < 4; ++side)
+        {
+            if (cellVertices[2 * side] != nullptr)
+            {
+
+                VoronoiDiagram::HalfEdge* halfEdge = mDiagram.createHalfEdge(mDiagram.getFace(i));
+                halfEdge->origin = cellVertices[2 * side]->vertex;
+                halfEdge->destination = cellVertices[2 * side + 1]->vertex;
+                cellVertices[2 * side]->nextHalfEdge = halfEdge;
+                halfEdge->prev = cellVertices[2 * side]->prevHalfEdge;
+                if (cellVertices[2 * side]->prevHalfEdge != nullptr)
+                    cellVertices[2 * side]->prevHalfEdge->next = halfEdge;
+                cellVertices[2 * side + 1]->prevHalfEdge = halfEdge;
+                halfEdge->next = cellVertices[2 * side + 1]->nextHalfEdge;
+                if (cellVertices[2 * side + 1]->nextHalfEdge != nullptr)
+                    cellVertices[2 * side + 1]->nextHalfEdge->prev = halfEdge;
+            }
         }
     }
-
+    return true;
 }
+
+
+
--- fortunes_alg/realization.h
+++ fortunes_alg/realization.h
@@ -1,17 +1,56 @@
 #ifndef LAB03_OOP_REALIZATION_H
 #define LAB03_OOP_REALIZATION_H
 
-#include "math.h"
 #include "data_structures.h"
-using namespace std;
 
-namespace bl = beachline;
+class FortuneAlgorithm
+{
+public:
+
+    FortuneAlgorithm(std::vector<Vector> points);
+    ~FortuneAlgorithm();
+
+    void construct();
+    bool bound(Field box);
+
+    VoronoiDiagram getDiagram();
+
+private:
+    VoronoiDiagram mDiagram;
+    Beachline mBeachline;
+    PriorityQueue<Event> mEvents;
+    double mBeachlineY;
 
 
-void build_voronoi (const vector<Point_Fortunes> &points,
-                   vector<bl::HalfEdgePtr> &halfedges,
-                   vector<bl::VertexPtr> &vertices,
-                   vector<bl::HalfEdgePtr> &faces);
+    void handleSiteEvent(Event* event);
+    void handleCircleEvent(Event* event);
 
+
+    Arc* breakArc(Arc* arc, VoronoiDiagram::Site* site);
+    void removeArc(Arc* arc, VoronoiDiagram::Vertex* vertex);
+
+
+    bool isMovingRight(const Arc* left, const Arc* right) const;
+    double getInitialX(const Arc* left, const Arc* right, bool movingRight) const;
+
+
+    void addEdge(Arc* left, Arc* right);
+    void setOrigin(Arc* left, Arc* right, VoronoiDiagram::Vertex* vertex);
+    void setDestination(Arc* left, Arc* right, VoronoiDiagram::Vertex* vertex);
+    void setPrevHalfEdge(VoronoiDiagram::HalfEdge* prev, VoronoiDiagram::HalfEdge* next);
+
+
+    void addEvent(Arc* left, Arc* middle, Arc* right);
+    void deleteEvent(Arc* arc);
+    Vector computeConvergencePoint(const Vector& point1, const Vector& point2, const Vector& point3, double& y) const;
+
+
+    struct LinkedVertex
+    {
+        VoronoiDiagram::HalfEdge* prevHalfEdge;
+        VoronoiDiagram::Vertex* vertex;
+        VoronoiDiagram::HalfEdge* nextHalfEdge;
+    };
+};
 
 #endif //LAB03_OOP_REALIZATION_H
